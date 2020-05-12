156068702970589c10f38bd9dc52769cbf434cfb
redhatvamsi <60828322+redhatvamsi@users.noreply.github.com>
2020-05-12 11:41:50 -0500

    Update github_to_morpheus.groovy

--- vars/github_to_morpheus.groovy
+++ vars/github_to_morpheus.groovy
@@ -4,16 +4,29 @@ import java.nio.file.Files
 def call(config){
     println "Printing from Pipeline call"
     node {
-        checkout([$class: 'GitSCM', 
-          branches: [[name: '*/master']], 
-          userRemoteConfigs: [[credentialsId: 'jenkins-gitlab', url: 'http://gitlab.example.com/redhatvamsi/morpheus-scripts.git']]])
+        dir('morpheus-scripts'){
+        git url: 'http://gitlab.example.com/cloud-team/morpheus-scripts.git',
+            credentialsId: "jenkins-gitlab", branch: 'master'
         
         def files = findFiles(glob: 'dev/*.sh') 
         files.each { item ->  
             println item.name+'\n'
             def data = readFile(file: 'dev/'+item.name)
             writeFile(file: 'prod/'+item.name, text: data)
-         }        
-        githubUpdate(config, 'update repo with changes') 
+         } 
+        try {
+          sh "git remote -v"
+          sh "git config user.email 'jenkins@example.com'"
+          sh "git config user.name 'redhatvamsi'"
+          sshagent(credentials: ['jenkins-gitlab']) {
+              //sh "cd morpheus-scripts"
+              sh "git add --all"
+              sh "git commit -m 'update through pipeline'"
+              sh('git push origin master')
+          }
+      } catch (e) {
+          //could be a noop
+         }
+        }
     }    
 }

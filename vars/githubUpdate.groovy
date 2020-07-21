9bac70a8c107086fe24ffec8123b64b91b3392bf
redhatvamsi <60828322+redhatvamsi@users.noreply.github.com>
2020-07-21 16:57:02 -0500

    Update githubUpdate.groovy

--- vars/githubUpdate.groovy
+++ vars/githubUpdate.groovy
@@ -1,19 +1,26 @@
-def call(config, commitMessage) {
-
+def call(commitMessage) {
+    dir('morpheus-pipeline-library') {
+      git url: 'git@gitlab.example.com:openshift/example-inventory.git',
+            credentialsId: "bitbucketsecret", branch: 'master'
+    }
+  
+    dir('morpheus-pipeline-library') {
       def noOp = true
       try {
-          sh "git commit -am 'update through pipeline'"
+          sh "git commit -am '${commitMessage}'"
           noOp = false
       } catch (e) {
           //could be a noop
       }
       if (!noOp) {
-          //sh "git remote -v"
-          sshagent(credentials: ['jenkins-gitlab']) {
+          sh "git remote -v"
+          sshagent(credentials: ['git-automation']) {
               sh('git push origin master')
           }
       }
     }
+}
 
 return this
 
+inventoryUpdate(' morpheus tasks list updated')
